daf6c0707ec4b2bf7e454e0cc23bd1c129ac4eb4
Zin Lin Phyo <zinlinphyo.mm@gmail.com>
2018-11-22 18:24:11 +0630

    add app icon & grouping folder

---
+++ MVC-ifyme/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVC-ifyme
+//
+//  Created by Zin Lin Phyo on 11/22/18.
+//  Copyright © 2018 Zin Lin Phyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
